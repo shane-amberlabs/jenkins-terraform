44be2a4f9ec33e3a0d7d53ff432de159946d998c
Shane Watson <shane.watson@amberlabs.io>
2022-11-23 13:22:48 +0000

    troubleshooting

--- scripts/create-state-s3.sh
+++ scripts/create-state-s3.sh
@@ -13,4 +13,4 @@ aws s3api put-public-access-block --bucket $S3_NAME --public-access-block-config
 
 aws s3api put-bucket-versioning --bucket $S3_NAME --versioning-configuration "Status=Enabled" --region $REGION
 
-aws dynamodb create-table --table-name $DYNAMODB_NAME --attribute-definitions AttributeName=LockID,AttributeType=S --key-schema AttributeName=LockID,KeyType=HASH --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 --region $REGION
+# aws dynamodb create-table --table-name $DYNAMODB_NAME --attribute-definitions AttributeName=LockID,AttributeType=S --key-schema AttributeName=LockID,KeyType=HASH --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 --region $REGION
